630006232a439cd064200bd7487eb42d4eaa25ca
ket <taxevaderket@gmail.com>
2022-03-05 18:50:34 +0100

    the 3rd commit and counting...

---
+++ main/sphereVolume.java
@@ -0,0 +1,24 @@
+package main;
+import java.util.Scanner;
+public class sphereVolume {
+    static double radius;
+    static double volume;
+    static String unit;
+    static Scanner input = new Scanner(System.in);
+    public static void main(String[] args) {
+        System.out.println("Enter the radius of your sphere.");
+        radius = input.nextDouble();
+        switch (unit) {
+            case "m":
+             volume = volume / 1000000;
+            break;
+
+            case "cm":
+             volume = volume - 0;
+            break;
+        }
+        volume = 4 / 3 * Math.PI * (radius * radius * radius);
+        unit = input.next();
+        input.close();
+    }
+}
