ef8f7dea9c5645050069dbe7093c65eff0587138
ket <taxevaderket@gmail.com>
2022-03-06 23:44:14 +0100

    added an auto runner

---
+++ coneRunner.cmd
@@ -0,0 +1,8 @@
+@echo off
+echo make sure you have the cloned file in your "USER" directory (assuming you are a windows users)
+cd c:\users\%username%\3DBodyCalculator\src\main
+:autoRun
+set /P c=Do You Want To Run The coneVolume Program?[Y/N]?
+if /I "%c%" EQU "Y" javac coneVolume.java && java coneVolume.java
+if /I "%c%" EQU "N" exit
+goto :autoRun
