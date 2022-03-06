2c5335e6d9651bcb74729baa43f922033bcd872c
ket <97126671+TaxEvaderKet@users.noreply.github.com>
2022-03-06 23:45:14 +0100

    Update coneRunner.cmd

--- coneRunner.cmd
+++ coneRunner.cmd
@@ -1,5 +1,6 @@
 @echo off
 echo make sure you have the cloned file in your "USER" directory (assuming you are a windows users)
+pause
 cd c:\users\%username%\3DBodyCalculator\src\main
 :autoRun
 set /P c=Do You Want To Run The coneVolume Program?[Y/N]?
