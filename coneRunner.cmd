5cd03301242859ac011c52750c167133839c68cd
ket <taxevaderket@gmail.com>
2022-03-07 08:13:40 +0100

    AAAAAAAAAA

--- coneRunner.cmd
+++ coneRunner.cmd
@@ -6,5 +6,4 @@ cd c:\users\%username%\3DBodyCalculator\src\main
 set /P c=Do You Want To Run The coneVolume Program?(Y/N)?
 if /I "%c%" EQU "Y" javac coneVolume.java && java coneVolume
 if /I "%c%" EQU "N" exit
-goto :autoRun
 exit
