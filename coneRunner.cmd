7acc526c7440978ead6450ad987c3e67feb92741
ket <taxevaderket@gmail.com>
2022-03-13 23:24:06 +0100

    removed the python equivalent since it was useless

--- coneRunner.cmd
+++
@@ -1,9 +0,0 @@
-@echo off
-echo make sure you have the cloned file in your "USER" directory (assuming you are a windows user)
-pause
-cd c:\users\%username%\3DBodyCalculator\src\main
-:autoRun
-set /P c=Do You Want To Run The coneVolume Program?(Y/N)?
-if /I "%c%" EQU "Y" javac coneVolume.java && java coneVolume
-if /I "%c%" EQU "N" exit
-:: I won't fix this, so send help please
